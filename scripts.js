408efaf2d1b898075c806aedd95c9fa75a368027
converterso12 <converterso12@gmail.com>
2024-05-24 06:16:00 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBgtPpDuHCLppJxrHUeLnyBDbhCOsq6QTA",
-  authDomain: "hitung-so2.firebaseapp.com",
-  projectId: "hitung-so2",
-  storageBucket: "hitung-so2.appspot.com",
-  messagingSenderId: "945221839072",
-  appId: "1:945221839072:web:a3354b470090122b534885",
-  measurementId: "G-012ZG1NG61"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBgtPpDuHCLppJxrHUeLnyBDbhCOsq6QTA",
+    authDomain: "hitung-so2.firebaseapp.com",
+    projectId: "hitung-so2",
+    storageBucket: "hitung-so2.appspot.com",
+    messagingSenderId: "945221839072",
+    appId: "1:945221839072:web:a3354b470090122b534885",
+    measurementId: "G-012ZG1NG61"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
